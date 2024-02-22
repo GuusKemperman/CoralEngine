43ef31e6962a15d762453dd60c143b84ae91eee1
Marcin Zalewski <marcinzal24@gmail.com>
2024-02-22 14:16:57 +0100

    MetaManager get type function fix

--- Include/Meta/MetaType.h
+++ Include/Meta/MetaType.h
@@ -601,6 +601,29 @@ namespace Engine
 		AddBaseClass(base);
 	}
 
+	template<typename T>
+	MetaType& MetaManager::GetType()
+	{
+		static_assert(sIsReflectable<T>, 
+		R"(Type does not have a reflect function, so the type can never be gotten from the MetaManager. 
+If it does have a Reflect function, make sure it is included from wherever this error originated.
+If you are trying to reflect an std::vector<std::shared_ptr<const Material>>, you need to include Material.h, ReflectVector.h and ReflectSmartPtr.h.)");
+
+		static constexpr TypeId typeId = MakeTypeId<T>();
+
+		if (MetaType* const existingType = TryGetType(typeId);
+			existingType != nullptr)
+		{
+			LIKELY;
+			return *existingType;
+		}
+
+		MetaType& newType = AddType(Internal::CallReflect<T>());
+		ASSERT(newType.GetTypeId() == MakeTypeId<T>() && "You have reflected a different type, check what you passed into MetaType constructor");
+		return newType;
+
+	}
+
 	namespace Internal
 	{
 		template<typename T, std::enable_if_t<sHasInternalReflect<T>, bool>>
