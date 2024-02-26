8de236e9f437994adf473a14564e961c382dccf9
Amália <220939@buas.nl>
2024-02-26 13:35:24 +0100

    Debug drawing

--- Include/Systems/PhysicsSystem2D.h
+++ Include/Systems/PhysicsSystem2D.h
@@ -10,6 +10,9 @@ namespace Engine
 		void Update(World& world, float dt) override;
 
 	private:
+		void UpdateTransforms(World& world, float dt);
+		void DebugDrawing(World& world);
+
 		friend ReflectAccess;
 		static MetaType Reflect();
 		REFLECT_AT_START_UP(PhysicsSystem2D);
--- Source/Systems/PhysicsSystem2D.cpp
+++ Source/Systems/PhysicsSystem2D.cpp
@@ -4,19 +4,47 @@
 #include "Components/Physics2D/PhysicsBody2DComponent.h"
 #include "Meta/MetaType.h"
 #include "Components/TransformComponent.h"
+#include "Components/Physics2D/DiskColliderComponent.h"
+#include "Components/Physics2D/PolygonColliderComponent.h"
 #include "World/Registry.h"
 #include "World/World.h"
+#include "World/WorldRenderer.h"
 
 void Engine::PhysicsSystem2D::Update(World& world, float dt)
 {
-	Registry& reg = world.GetRegistry();
-	auto view = reg.View<PhysicsBody2DComponent, TransformComponent>();
-	for (auto [entity, body, transform] : view.each())
-	{
-		body.Update(dt);
+    UpdateTransforms(world, dt);
+    DebugDrawing(world);
+}
 
-		transform.SetLocalPosition({ body.mPosition.x, transform.GetLocalPosition().y, body.mPosition.y});
-	}
+void Engine::PhysicsSystem2D::UpdateTransforms(World& world, float dt)
+{
+    Registry& reg = world.GetRegistry();
+    auto view = reg.View<PhysicsBody2DComponent, TransformComponent>();
+    for (auto [entity, body, transform] : view.each())
+    {
+        body.Update(dt);
+
+        transform.SetLocalPosition({ body.mPosition.x, transform.GetLocalPosition().y, body.mPosition.y });
+    }
+}
+
+void Engine::PhysicsSystem2D::DebugDrawing(World& world)
+{
+#ifdef _DEBUG
+    Registry& reg = world.GetRegistry();
+    const auto diskView = reg.View<DiskColliderComponent, TransformComponent>();
+    const auto& renderer = world.GetRenderer();
+    for (auto [entity, disk, transformComponent] : diskView.each())
+    {
+        renderer.AddCircle(
+            DebugCategory::All, transformComponent.GetWorldPosition(), disk.mRadius + 0.1f, { 1.0f, 0.0f, 0.0f, 1.0f });
+    }
+    const auto polyView = reg.View<PolygonColliderComponent, TransformComponent>();
+    for (auto [entity, poly, transformComponent] : polyView.each())
+    {
+        renderer.AddPolygon(DebugCategory::All, poly.mPoints, { 1.0f, 0.0f, 0.0f, 1.0f});
+    }
+#endif
 }
 
 Engine::MetaType Engine::PhysicsSystem2D::Reflect()
