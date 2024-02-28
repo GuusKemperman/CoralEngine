8657b59fb52b7989fe5bf1770fc000c2eef9a188
god-of-errors <73073634+god-of-errors@users.noreply.github.com>
2024-02-28 10:45:01 +0100

    The Tick Priority of the Physics System

--- Include/Systems/PhysicsSystem2D.h
+++ Include/Systems/PhysicsSystem2D.h
@@ -14,6 +14,13 @@ namespace Engine
 	public:
 		void Update(World& world, float dt) override;
 
+		SystemStaticTraits GetStaticTraits() const override
+		{
+			SystemStaticTraits traits{};
+			traits.mPriority = static_cast<int>(TickPriorities::Physics);
+			return traits;
+		}
+
 	private:
 		friend Physics2DUnitTestAccess;
 
@@ -25,12 +32,19 @@ namespace Engine
 		static void ResolveCollision(const CollisionData& collision, PhysicsBody2DComponent& body1,
 		                             PhysicsBody2DComponent& body2);
 
-		static void RegisterCollision(CollisionData& collision, const entt::entity& entity1, PhysicsBody2DComponent& body1, const entt::entity& entity2, PhysicsBody2DComponent& body2);
-		static bool CollisionCheckDiskDisk(const glm::vec2& center1, float radius1, const glm::vec2& center2, float radius2, CollisionData& result);
-		static bool CollisionCheckDiskPolygon(const glm::vec2& diskCenter, float diskRadius, const glm::vec2& polygonPos, const std::vector<glm::vec2>& polygonPoints, CollisionData& result);
+		static void RegisterCollision(CollisionData& collision, const entt::entity& entity1,
+		                              PhysicsBody2DComponent& body1, const entt::entity& entity2,
+		                              PhysicsBody2DComponent& body2);
+		static bool CollisionCheckDiskDisk(const glm::vec2& center1, float radius1, const glm::vec2& center2,
+		                                   float radius2, CollisionData& result);
+		static bool CollisionCheckDiskPolygon(const glm::vec2& diskCenter, float diskRadius,
+		                                      const glm::vec2& polygonPos, const std::vector<glm::vec2>& polygonPoints,
+		                                      CollisionData& result);
 		static bool IsPointInsidePolygon(const glm::vec2& point, const std::vector<glm::vec2>& polygon);
-		static glm::vec2 GetNearestPointOnPolygonBoundary(const glm::vec2& point, const std::vector<glm::vec2>& polygon);
-		static glm::vec2 GetNearestPointOnLineSegment(const glm::vec2& p, const glm::vec2& segmentA, const glm::vec2& segmentB);
+		static glm::vec2
+		GetNearestPointOnPolygonBoundary(const glm::vec2& point, const std::vector<glm::vec2>& polygon);
+		static glm::vec2 GetNearestPointOnLineSegment(const glm::vec2& p, const glm::vec2& segmentA,
+		                                              const glm::vec2& segmentB);
 
 		friend ReflectAccess;
 		static MetaType Reflect();
