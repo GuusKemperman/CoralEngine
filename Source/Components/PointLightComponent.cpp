d2761d0c226cdbcb3772191986675d85a5db4ea8
Marcin Zalewski <marcinzal24@gmail.com>
2024-03-01 09:19:25 +0100

    Added point light component

---
+++ Source/Components/PointLightComponent.cpp
@@ -0,0 +1,20 @@
+#include "Precomp.h"
+#include "Components/PointLightComponent.h"
+
+#include "Meta/MetaProps.h"
+#include "Meta/MetaType.h"
+#include "Utilities/Reflect/ReflectComponentType.h"
+
+Engine::MetaType Engine::PointLightComponent::Reflect()
+{
+	MetaType metaType = MetaType{ MetaType::T<PointLightComponent>{}, "PointLightComponent" };
+	metaType.GetProperties().Add(Props::sIsScriptableTag);
+
+	metaType.AddField(&PointLightComponent::mColor, "mColor").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&PointLightComponent::mIntensity, "mIntensity").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&PointLightComponent::mRange, "mRange").GetProperties().Add(Props::sIsScriptableTag);
+
+	ReflectComponentType<PointLightComponent>(metaType);
+
+	return metaType;
+}
