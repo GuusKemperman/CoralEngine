d86d5ed229a82bca7e90ff7f64e6900b4ef93dde
Amália <220939@buas.nl>
2024-03-04 13:09:31 +0100

    Ability component

---
+++ Include/Components/Abilities/AbilityComponent.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Meta/MetaReflect.h"
+
+namespace Engine
+{
+	class Registry;
+
+	class AbilityComponent
+	{
+	public:
+		std::string mIconTextureName{};
+		std::string mDescription{};
+
+		enum Target
+		{
+			Hostile,
+			Self,
+			Friendly,
+			SelfAndFriendly
+		}mTarget{};
+
+		bool mGCD = true; // whether this ability takes into account the global cooldown
+
+		enum RequirementType
+		{
+			Cooldown,
+			Kills
+		}mRequirementType{};
+		float mRequirementToUse{};
+		float mRequirementCounter{};
+		int mCharges{};
+		int mCurrentCharges{};
+
+	private:
+		friend ReflectAccess;
+		static MetaType Reflect();
+		REFLECT_AT_START_UP(AbilityComponent);
+	};
+}
+
+template<>
+struct Reflector<Engine::AbilityComponent::Target>
+{
+	static Engine::MetaType Reflect();
+	static constexpr bool sIsSpecialized = true;
+}; REFLECT_AT_START_UP(Target, Engine::AbilityComponent::Target);
+
+template<>
+struct Engine::EnumStringPairsImpl<Engine::AbilityComponent::Target>
+{
+	static constexpr EnumStringPairs<AbilityComponent::Target, 4> value = {
+		EnumStringPair<AbilityComponent::Target>{ AbilityComponent::Target::Hostile, "Hostile" },
+		{ AbilityComponent::Target::Self, "Self" },
+		{ AbilityComponent::Target::Friendly, "Friendly" },
+		{ AbilityComponent::Target::SelfAndFriendly, "SelfAndFriendly" },
+	};
+};
+
+template<>
+struct Reflector<Engine::AbilityComponent::RequirementType>
+{
+	static Engine::MetaType Reflect();
+	static constexpr bool sIsSpecialized = true;
+}; REFLECT_AT_START_UP(RequirementType, Engine::AbilityComponent::RequirementType);
+
+template<>
+struct Engine::EnumStringPairsImpl<Engine::AbilityComponent::RequirementType>
+{
+	static constexpr EnumStringPairs<AbilityComponent::RequirementType, 2> value = {
+		EnumStringPair<AbilityComponent::RequirementType>{ AbilityComponent::RequirementType::Cooldown, "Cooldown" },
+		{ AbilityComponent::RequirementType::Kills, "Kills" },
+	};
+};
---
+++ Source/Components/Abilities/AbilityComponent.cpp
@@ -0,0 +1,59 @@
+#include "Precomp.h"
+#include "Components/Abilities/AbilityComponent.h"
+
+#include "Meta/MetaType.h"
+#include "Meta/MetaProps.h"
+#include "Utilities/Reflect/ReflectComponentType.h"
+#include "Utilities/Reflect/ReflectFieldType.h"
+
+Engine::MetaType Engine::AbilityComponent::Reflect()
+{
+	MetaType metaType = MetaType{ MetaType::T<AbilityComponent>{}, "AbilityComponent" };
+	metaType.GetProperties().Add(Props::sIsScriptableTag);
+
+	metaType.AddField(&AbilityComponent::mDescription, "mDescription").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&AbilityComponent::mIconTextureName, "mIconTextureName").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&AbilityComponent::mTarget, "mTarget").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&AbilityComponent::mGCD, "mGCD").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&AbilityComponent::mRequirementType, "mRequirementType").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&AbilityComponent::mRequirementToUse, "mRequirementToUse").GetProperties().Add(Props::sIsScriptableTag);
+	auto& requirementCounterProps = metaType.AddField(&AbilityComponent::mRequirementCounter, "mRequirementCounter").GetProperties();
+	requirementCounterProps.Add(Props::sIsScriptableTag);
+	requirementCounterProps.Add(Props::sNoInspectTag);
+	metaType.AddField(&AbilityComponent::mCharges, "mCharges").GetProperties().Add(Props::sIsScriptableTag);
+	auto& currentChargesProps = metaType.AddField(&AbilityComponent::mCurrentCharges, "mCurrentCharges").GetProperties();
+	currentChargesProps.Add(Props::sIsScriptableTag);
+	currentChargesProps.Add(Props::sNoInspectTag);
+
+	ReflectComponentType<AbilityComponent>(metaType);
+
+	return metaType;
+}
+
+Engine::MetaType Reflector<Engine::AbilityComponent::Target>::Reflect()
+{
+	using namespace Engine;
+	using T = AbilityComponent::Target;
+	MetaType type{ MetaType::T<T>{}, "Ability Target" };
+
+	type.GetProperties().Add(Props::sIsScriptableTag);
+	type.AddFunc(std::equal_to<T>(), OperatorType::equal, MetaFunc::ExplicitParams<const T&, const T&>{}).GetProperties().Add(Props::sIsScriptableTag);
+	type.AddFunc(std::not_equal_to<T>(), OperatorType::inequal, MetaFunc::ExplicitParams<const T&, const T&>{}).GetProperties().Add(Props::sIsScriptableTag);
+	ReflectFieldType<T>(type);
+
+	return type;
+}
+
+Engine::MetaType Reflector<Engine::AbilityComponent::RequirementType>::Reflect()
+{
+	using namespace Engine;
+	using T = AbilityComponent::RequirementType;
+	MetaType type{ MetaType::T<T>{}, "Ability RequirementType" };
+
+	type.GetProperties().Add(Props::sIsScriptableTag);
+	type.AddFunc(std::equal_to<T>(), OperatorType::equal, MetaFunc::ExplicitParams<const T&, const T&>{}).GetProperties().Add(Props::sIsScriptableTag);
+	type.AddFunc(std::not_equal_to<T>(), OperatorType::inequal, MetaFunc::ExplicitParams<const T&, const T&>{}).GetProperties().Add(Props::sIsScriptableTag);
+	ReflectFieldType<T>(type);
+
+	return type;
+}
