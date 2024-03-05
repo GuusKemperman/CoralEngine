b314f8a3720044b2243e2addd9b37981bf900fe7
GuusKemperman <guus.kemperman@gmail.com>
2024-03-05 15:31:12 +0100

    Added AI events

--- Include/Utilities/Events.h
+++ Include/Utilities/Events.h
@@ -9,20 +9,37 @@ namespace Engine
 	class EventBase
 	{
 	public:
+		constexpr EventBase(std::string_view name, bool isPure);
+
 		std::string_view mName{};
+		bool mIsPure{};
 	};
 
-	template<typename T>
+	template<typename T, bool IsPure = false>
 	class Event
 	{
 		static_assert(AlwaysFalse<T>, "Not a signature");
 	};
 
-	template<typename Ret, typename... Args>
-	class Event<Ret(Args...)> :
+	template<typename Ret, typename... Args, bool IsPure>
+	class Event<Ret(Args...), IsPure> :
 		public EventBase
 	{
+	public:
+		constexpr Event(std::string_view name);
 	};
+
+	constexpr EventBase::EventBase(std::string_view name, bool isPure) :
+		mName(name),
+		mIsPure(isPure)
+	{
+	}
+
+	template <typename Ret, typename ... Args, bool IsPure>
+	constexpr Event<Ret(Args...), IsPure>::Event(std::string_view name) :
+		EventBase(name, IsPure)
+	{
+	}
 
 	//********************************//
 	//				API				  //
@@ -35,6 +52,10 @@ namespace Engine
 	{
 		static constexpr std::string_view sIsEventStaticTag = "sIsEventStaticTag";
 	}
+
+	static constexpr Event<float(const World&, entt::entity), true> sAIEvaluateEvent{ "OnAIEvaluate" };
+
+	static constexpr Event<void(World&, entt::entity, float)> sAITickEvent{ "OnAITick" };
 
 	/**
 	 * \brief Called every frame.
@@ -82,42 +103,46 @@ namespace Engine
 	 *		return type;
 	 *	}
 	 */
-	template<typename Class, typename Func, typename Ret, typename... Args>
-	void BindEvent(MetaType& type, const Event<Ret(Args...)>& event, Func&& func);
+	template<typename Class, typename Func, typename Ret, typename... Args, bool IsPure>
+	void BindEvent(MetaType& type, const Event<Ret(Args...), IsPure>& event, Func&& func);
 
 	/**
 	 * \brief An overload that prevents having to specify Class for mutable member functions
 	 */
-	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(FuncObj::* func)(FuncParams...));
+	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(FuncObj::* func)(FuncParams...));
 
 	/**
 	 * \brief An overload that prevents having to specify Class for const member functions
 	 */
-	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(FuncObj::* func)(FuncParams...) const);
+	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(FuncObj::* func)(FuncParams...) const);
 
-	template<typename FuncRet, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(*func)(FuncParams...));
+	template<typename FuncRet, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(*func)(FuncParams...));
 
 	/**
 	 * \brief Returns the event bound during BindEvent, if any.
 	 */
-	template<typename... Args>
-	const MetaFunc* TryGetEvent(const MetaType& fromType, const Event<Args...>& event);
+	template<typename T>
+	const MetaFunc* TryGetEvent(const MetaType& fromType, const Event<T>& event);
 
 	//********************************//
 	//			Implementation		  //
 	//********************************//
+
+
 
 	namespace Internal
 	{
 		static constexpr std::string_view sIsEventProp = "IsEvent";
 	}
 
-	template<typename Class, typename Func, typename Ret, typename... Args>
-	void BindEvent(MetaType& type, const Event<Ret(Args...)>& event, Func&& func)
+	template<typename Class, typename Func, typename Ret, typename... Args, bool IsPure>
+	void BindEvent(MetaType& type, const Event<Ret(Args...), IsPure>& event, Func&& func)
 	{
+		static_assert(std::is_const_v<Class> == IsPure, "Cannot be bound to function, make the function const (or remove const)");
+
 		MetaFunc* eventFunc{};
 
 		if constexpr (!sIsEventStatic<Class>)
@@ -131,35 +156,30 @@ namespace Engine
 			eventFunc = &type.AddFunc(std::function<Ret(Args...)>{ std::forward<Func>(func) }, event.mName);
 			eventFunc->GetProperties().Add(Props::sIsEventStaticTag);
 		}
-		eventFunc->GetProperties().Add(Internal::sIsEventProp);
+		eventFunc->GetProperties().Add(Internal::sIsEventProp).Set(Props::sIsScriptPure, IsPure);
 	}
 
-	/**
-	 * \brief An overload that prevents having to specify Class for mutable member functions
-	 */
-	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(FuncObj::* func)(FuncParams...))
+
+	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(FuncObj::* func)(FuncParams...))
 	{
 		BindEvent<FuncObj>(type, event, func);
 	}
 
-	/**
-	 * \brief An overload that prevents having to specify Class for const member functions
-	 */
-	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(FuncObj::* func)(FuncParams...) const)
+	template<typename FuncRet, typename FuncObj, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(FuncObj::* func)(FuncParams...) const)
 	{
 		BindEvent<FuncObj>(type, event, func);
 	}
 
-	template<typename FuncRet, typename... FuncParams, typename EventRet, typename... EventParams>
-	void BindEvent(MetaType& type, const Event<EventRet(EventParams...)>& event, FuncRet(*func)(FuncParams...))
+	template<typename FuncRet, typename... FuncParams, typename EventT>
+	void BindEvent(MetaType& type, const EventT& event, FuncRet(*func)(FuncParams...))
 	{
 		BindEvent<std::monostate>(type, event, func);
 	}
 
-	template <typename ... Args>
-	const MetaFunc* TryGetEvent(const MetaType& fromType, const Event<Args...>& event)
+	template <typename T>
+	const MetaFunc* TryGetEvent(const MetaType& fromType, const Event<T>& event)
 	{
 		const MetaFunc* func = fromType.TryGetFunc(event.mName);
 
--- Source/Scripting/ScriptEvents.cpp
+++ Source/Scripting/ScriptEvents.cpp
@@ -4,14 +4,15 @@
 #include "Core/VirtualMachine.h"
 #include "Scripting/ScriptFunc.h"
 
-Engine::ScriptEvent::ScriptEvent(const EventBase& event, std::vector<MetaFuncNamedParam>&& params) :
+Engine::ScriptEvent::ScriptEvent(const EventBase& event, std::vector<MetaFuncNamedParam>&& params, std::optional<MetaFuncNamedParam>&& ret) :
 	mBasedOnEvent(event),
-	mParamsToShowToUser(std::move(params))
+	mParamsToShowToUser(std::move(params)),
+	mReturnValueToShowToUser(std::move(ret))
 {
 }
 
 Engine::ScriptTickEvent::ScriptTickEvent() :
-	ScriptEvent(sTickEvent, { { MakeTypeTraits<float>(), "DeltaTime" } })
+	ScriptEvent(sTickEvent, { { MakeTypeTraits<float>(), "DeltaTime" } }, std::nullopt)
 {
 }
 
@@ -54,7 +55,7 @@ void Engine::ScriptTickEvent::Define(MetaFunc& declaredFunc, const ScriptFunc& s
 }
 
 Engine::ScriptFixedTickEvent::ScriptFixedTickEvent() :
-	ScriptEvent(sFixedTickEvent, { { MakeTypeTraits<float>(), "DeltaTime" } })
+	ScriptEvent(sFixedTickEvent, { { MakeTypeTraits<float>(), "DeltaTime" } }, std::nullopt)
 {
 }
 
@@ -95,3 +96,97 @@ void Engine::ScriptFixedTickEvent::Define(MetaFunc& declaredFunc, const ScriptFu
 			return VirtualMachine::Get().ExecuteScriptFunction(scriptArgs, rvoBuffer, scriptFunc, firstNode, entry);
 		});
 }
+
+Engine::ScriptAITickEvent::ScriptAITickEvent() :
+	ScriptEvent(sAITickEvent, { { MakeTypeTraits<float>(), "DeltaTime" } }, std::nullopt)
+{
+}
+
+Engine::MetaFunc& Engine::ScriptAITickEvent::Declare(TypeTraits selfTraits, MetaType& toType) const
+{
+	static_assert(std::is_same_v<const Event<void(World&, entt::entity, float)>, decltype(sAITickEvent)>);
+
+	std::vector<MetaFuncNamedParam> metaParams
+	{
+		{ selfTraits },
+		{ MakeTypeTraits<World&>() },
+		{ MakeTypeTraits<entt::entity>() },
+		{ MakeTypeTraits<float>() },
+	};
+
+	MetaFuncNamedParam metaReturn{ MakeTypeTraits<void>() };
+
+	MetaFunc& func = toType.AddFunc([](MetaFunc::DynamicArgs, MetaFunc::RVOBuffer) -> FuncResult
+		{
+			return { "There were unresolved compilation errors" };
+		},
+		mBasedOnEvent.get().mName,
+		metaReturn,
+		metaParams
+	);
+
+	func.GetProperties().Add(Internal::sIsEventProp);
+
+	return func;
+}
+
+void Engine::ScriptAITickEvent::Define(MetaFunc& declaredFunc, const ScriptFunc& scriptFunc,
+	std::shared_ptr<const Script> script) const
+{
+	declaredFunc.RedirectFunction([&scriptFunc, script, firstNode = scriptFunc.GetFirstNode().GetValue(), entry = scriptFunc.GetEntryNode().GetValue()]
+	(MetaFunc::DynamicArgs args, MetaFunc::RVOBuffer rvoBuffer) -> FuncResult
+		{
+			// The reference to the component and the deltatime
+			std::array<MetaAny, 2> scriptArgs{ std::move(args[0]), std::move(args[3]) };
+			return VirtualMachine::Get().ExecuteScriptFunction(scriptArgs, rvoBuffer, scriptFunc, firstNode, entry);
+		});
+}
+
+Engine::ScriptAIEvaluateEvent::ScriptAIEvaluateEvent() :
+	ScriptEvent(sAIEvaluateEvent, {}, MetaFuncNamedParam{ MakeTypeTraits<float>(), "Score" })
+{
+}
+
+Engine::MetaFunc& Engine::ScriptAIEvaluateEvent::Declare(TypeTraits selfTraits, MetaType& toType) const
+{
+	static_assert(std::is_same_v<const Event<float(const World&, entt::entity), true>, decltype(sAIEvaluateEvent)>);
+
+	selfTraits.mForm = TypeForm::ConstRef;
+
+	std::vector<MetaFuncNamedParam> metaParams
+	{
+		{ selfTraits },
+		{ MakeTypeTraits<const World&>() },
+		{ MakeTypeTraits<entt::entity>() },
+	};
+
+	MetaFuncNamedParam metaReturn{ MakeTypeTraits<float>() };
+
+	MetaFunc& func = toType.AddFunc([](MetaFunc::DynamicArgs, MetaFunc::RVOBuffer) -> FuncResult
+		{
+			return { "There were unresolved compilation errors" };
+		},
+		mBasedOnEvent.get().mName,
+		metaReturn,
+		metaParams
+	);
+
+	func.GetProperties().Add(Internal::sIsEventProp);
+	func.GetProperties().Set(Props::sIsScriptPure, true);
+
+	return func;
+}
+
+void Engine::ScriptAIEvaluateEvent::Define(MetaFunc& declaredFunc, const ScriptFunc& scriptFunc,
+	std::shared_ptr<const Script> script) const
+{
+	declaredFunc.RedirectFunction([&scriptFunc, script, firstNode = scriptFunc.GetFirstNode().GetValue(), entry = scriptFunc.GetEntryNode().GetValue()]
+	(MetaFunc::DynamicArgs, MetaFunc::RVOBuffer rvoBuffer) -> FuncResult
+		{
+			// The component already has the world
+			// and it's owner
+			Span<MetaAny, 0> scriptArgs{};
+			return VirtualMachine::Get().ExecuteScriptFunction(scriptArgs, rvoBuffer, scriptFunc, firstNode, entry);
+		});
+}
+
