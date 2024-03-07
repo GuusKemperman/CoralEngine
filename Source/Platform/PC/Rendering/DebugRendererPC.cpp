a1aab40f945518ae1eda8d5d5968f34339c2915e
Marcin Zalewski <marcinzal24@gmail.com>
2024-03-07 11:28:17 +0100

    Fixed PC build

--- Source/Platform/PC/Rendering/DebugRendererPC.cpp
+++ Source/Platform/PC/Rendering/DebugRendererPC.cpp
@@ -1,6 +1,11 @@
 #include "Precomp.h"
 #include "Utilities/DebugRenderer.h"
 
+#include "World/World.h"
+#include "World/Registry.h"
+#include "Components/TransformComponent.h"
+#include "Components/CameraComponent.h"
+
 class Engine::DebugRenderer::Impl
 {
 public:
@@ -18,31 +23,31 @@ Engine::DebugRenderer::~DebugRenderer()
 {
 }
 
-void Engine::DebugRenderer::AddLine(const glm::vec3& from, const glm::vec3& to, const glm::vec4& color)
+void Engine::DebugRenderer::AddLine(DebugCategory::Enum category, const glm::vec3& from, const glm::vec3& to, const glm::vec4& color) const
 {
+    if (!(sDebugCategoryFlags & category)) return;
+    mImpl->AddLine(from, to, color);
 }
 
-void Engine::DebugRenderer::AddSphere(const glm::vec3& center, float radius, const glm::vec4& color)
+void Engine::DebugRenderer::Render(const World& world)
 {
-}
+    const auto cameraView = world.GetRegistry().View<const TransformComponent, const CameraComponent>();
 
-void Engine::DebugRenderer::AddCube(const glm::vec3& center, float size)
-{
-}
-
-void Engine::DebugRenderer::AddPlane(const glm::vec3& p1, const glm::vec3& p2)
-{
+    for (auto [entity, cameraTransform, camera] : cameraView.each())
+    {
+        mImpl->Render(camera.GetView(), camera.GetProjection());
+    }
 }
 
 Engine::DebugRenderer::Impl::Impl()
 {
 }
 
-bool Engine::DebugRenderer::Impl::AddLine(const glm::vec3& from, const glm::vec3& to, const glm::vec4& color)
+bool Engine::DebugRenderer::Impl::AddLine(const glm::vec3&, const glm::vec3&, const glm::vec4&)
 {
     return false;
 }
 
-void Engine::DebugRenderer::Impl::Render(const glm::mat4& view, const glm::mat4& projection)
+void Engine::DebugRenderer::Impl::Render(const glm::mat4&, const glm::mat4&)
 {
 }
