a6ec4d13fa9af4c9d7e6d7082ad1f47657c9e1a1
GuusKemperman <guus.kemperman@gmail.com>
2024-03-08 17:16:39 +0100

    Real world conditions

--- Source/Core/Engine.cpp
+++ Source/Core/Engine.cpp
@@ -17,53 +17,49 @@
 #include "Utilities/Benchmark.h"
 #include "World/Registry.h"
 
-Engine::EngineClass::EngineClass(int, char*[], std::string_view)
+Engine::EngineClass::EngineClass(int argc, char* argv[], std::string_view gameDir)
 {
-	std::cout << "Hello world!" << std::endl;
-	exit(EXIT_FAILURE);
+	FileIO::StartUp(argc, argv, gameDir);
+	Logger::StartUp();
+	Device::StartUp();
+	Input::StartUp();
+#ifdef PLATFORM_WINDOWS
+	Device::Get().CreateImguiContext();
+#endif
+	MetaManager::StartUp();
+	AssetManager::StartUp();
+	VirtualMachine::StartUp();
 
-//	FileIO::StartUp(argc, argv, gameDir);
-//	Logger::StartUp();
-//	Device::StartUp();
-//	Input::StartUp();
-//#ifdef PLATFORM_WINDOWS
-//	Device::Get().CreateImguiContext();
-//#endif
-//	MetaManager::StartUp();
-//	AssetManager::StartUp();
-//	VirtualMachine::StartUp();
-//
-//#ifdef EDITOR
-//	Editor::StartUp();
-//	UnitTestManager::StartUp();
-//
-//	if (argc >= 2
-//		&& strcmp(argv[1], "run_tests") == 0)
-//	{
-//		exit(0);
-//
-//		//uint32 numFailed = 0;
-//		//for (UnitTest& test : UnitTestManager::Get().GetAllTests())
-//		//{
-//		//	test();
-//		//	if (test.mResult != UnitTest::Success)
-//		//	{
-//		//		numFailed++;
-//		//	}
-//		//}
-//
-//		//// We only exit if numFailed != 0,
-//		//// since maybe theres a crash in
-//		//// the shutdown process and we want
-//		//// to test that as well
-//		//if (numFailed != 0)
-//		//{
-//		//	exit(0);
-//		//}
-//	}
-//	exit(55);
-//
-//#endif // !EDITOR
+#ifdef EDITOR
+	Editor::StartUp();
+	UnitTestManager::StartUp();
+
+	if (argc >= 2
+		&& strcmp(argv[1], "run_tests") == 0)
+	{
+		uint32 numFailed = 0;
+		for (UnitTest& test : UnitTestManager::Get().GetAllTests())
+		{
+			test();
+			if (test.mResult != UnitTest::Success)
+			{
+				numFailed++;
+			}
+		}
+
+		// We only exit if numFailed != 0,
+		// since maybe theres a crash in
+		// the shutdown process and we want
+		// to test that as well
+		if (numFailed != 0)
+		{
+			// A lot of exits lead to exit(1).
+			// by doing + 1 we can distinguish
+			// from those errors
+			exit(numFailed + 1);
+		}
+	}
+#endif // !EDITOR
 }
 
 Engine::EngineClass::~EngineClass()
