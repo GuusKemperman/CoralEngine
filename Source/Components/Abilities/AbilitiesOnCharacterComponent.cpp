81fb17c78c011b181deb665ef6b5e1edac5e2eca
Amália <220939@buas.nl>
2024-03-12 09:53:20 +0100

    Added EDITOR checks for On Inspect()

--- Source/Components/Abilities/AbilitiesOnCharacterComponent.cpp
+++ Source/Components/Abilities/AbilitiesOnCharacterComponent.cpp
@@ -19,13 +19,16 @@ Engine::MetaType Engine::AbilitiesOnCharacterComponent::Reflect()
 	metaType.AddField(&AbilitiesOnCharacterComponent::mIsPlayer, "mIsPlayer").GetProperties().Add(Props::sIsScriptableTag).Add(Props::sNoInspectTag);
 	metaType.AddField(&AbilitiesOnCharacterComponent::mAbilitiesToInput, "mAbilitiesToInput").GetProperties().Add(Props::sNoInspectTag);
 
+#ifdef EDITOR
 	BindEvent(metaType, sInspectEvent, &AbilitiesOnCharacterComponent::OnInspect);
+#endif // EDITOR
 
 	ReflectComponentType<AbilitiesOnCharacterComponent>(metaType);
 
 	return metaType;
 }
 
+#ifdef EDITOR
 static bool isPlayer = true; // little hack to inspect the component conditionally
 void Engine::AbilitiesOnCharacterComponent::OnInspect(World& world, const std::vector<entt::entity>& entities)
 {
@@ -44,6 +47,7 @@ void Engine::AbilitiesOnCharacterComponent::OnInspect(World& world, const std::v
 		ShowInspectUI("Abilities", abilities.mAbilitiesToInput);
 	}
 }
+#endif // EDITOR
 
 bool Engine::AbilityInstance::operator==(const AbilityInstance& other) const
 {
