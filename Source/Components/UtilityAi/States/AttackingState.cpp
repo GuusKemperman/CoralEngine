ca2e873881adf98dbf7f50e7ad358898d3cc7cc8
god-of-errors <73073634+god-of-errors@users.noreply.github.com>
2024-03-13 13:17:30 +0100

    Added functionality to the Attacking State

--- Source/Components/UtilityAi/States/AttackingState.cpp
+++ Source/Components/UtilityAi/States/AttackingState.cpp
@@ -2,14 +2,36 @@
 #include "Components/UtililtyAi/States/AttackingState.h"
 
 #include "Components/TransformComponent.h"
+#include "Components/Abilities/CharacterComponent.h"
+#include "Components/Abilities/AbilitiesOnCharacterComponent.h"
 #include "Components/Pathfinding/NavMeshAgentComponent.h"
 #include "Components/Pathfinding/NavMeshTargetComponent.h"
+#include "Systems/AbilitySystem.h"
 #include "Meta/MetaType.h"
 #include "Utilities/Events.h"
 #include "Utilities/Reflect/ReflectComponentType.h"
 
 void Engine::AttackingState::OnAiTick(World& world, entt::entity owner, float)
 {
+	const auto characterData = world.GetRegistry().TryGet<CharacterComponent>(owner);
+	if (characterData == nullptr)
+	{
+		return;
+	}
+
+	const auto abilities = world.GetRegistry().TryGet<AbilitiesOnCharacterComponent>(owner);
+	if (abilities == nullptr)
+	{
+		return;
+	}
+
+	if (abilities->mAbilitiesToInput.empty())
+	{
+		return;
+	}
+
+	AbilitySystem::ActivateAbility(world, owner, *characterData, abilities->mAbilitiesToInput[0]);
+
 	auto* navMeshAgent = world.GetRegistry().TryGet<NavMeshAgentComponent>(owner);
 
 	if (navMeshAgent == nullptr) { return; }
