c93cf6ab0faa3d016bdcf52dee6af295cb3ec996
Amália <220939@buas.nl>
2024-03-18 13:17:47 +0100

    Minor changes

--- Source/Components/Physics2D/PhysicsBody2DComponent.cpp
+++ Source/Components/Physics2D/PhysicsBody2DComponent.cpp
@@ -32,7 +32,7 @@ void Engine::CollisionRules::DisplayWidget(const std::string& name)
 
 		ShowInspectUI("Object type", mLayer);
 
-		static constexpr uint32 numOfResponses = static_cast<uint32>(CollisionResponse::NUM_OF_RESPONSES);
+		static constexpr int numOfResponses = static_cast<int>(CollisionResponse::NUM_OF_RESPONSES);
 
 		if (ImGui::BeginTable("##ResponseTable", numOfResponses + 1))
 		{
@@ -45,7 +45,7 @@ void Engine::CollisionRules::DisplayWidget(const std::string& name)
 				ImGui::TextUnformatted(str.data());
 			}
 
-			for (int layer = 0, id = 213456; layer < static_cast<uint32>(CollisionLayer::NUM_OF_LAYERS); layer++)
+			for (int layer = 0, id = 213456; layer < static_cast<int>(CollisionLayer::NUM_OF_LAYERS); layer++)
 			{
 				ImGui::TableNextRow();
 				ImGui::TableSetColumnIndex(0);
@@ -79,7 +79,7 @@ Engine::MetaType Engine::PhysicsBody2DComponent::Reflect()
 	MetaType metaType = MetaType{ MetaType::T<PhysicsBody2DComponent>{}, "PhysicsBody2DComponent" };
 	metaType.GetProperties().Add(Props::sIsScriptableTag);
 
-	metaType.AddField(&PhysicsBody2DComponent::mRules, "mRules").GetProperties().Add(Props::sIsScriptableTag);
+	metaType.AddField(&PhysicsBody2DComponent::mRules, "mCollisionRules").GetProperties().Add(Props::sIsScriptableTag);
 	metaType.AddField(&PhysicsBody2DComponent::mIsAffectedByForces, "mIsAffectedByForces").GetProperties().Add(Props::sIsScriptableTag);
 	metaType.AddField(&PhysicsBody2DComponent::mInvMass, "mInvMass").GetProperties().Add(Props::sIsScriptableTag);
 	metaType.AddField(&PhysicsBody2DComponent::mRestitution, "mRestitution").GetProperties().Add(Props::sIsScriptableTag);
