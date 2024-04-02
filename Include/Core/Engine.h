af518e1bb737fde37bc60929f1bb9861f87f8f86
GuusKemperman <guus.kemperman@gmail.com>
2024-04-02 09:10:43 +0200

    Renamed EngineClass to Engine

--- Include/Core/Engine.h
+++ Include/Core/Engine.h
@@ -2,11 +2,11 @@
 
 namespace CE
 {
-	class EngineClass
+	class Engine
 	{
 	public:
-		EngineClass(int argc, char* argv[], std::string_view gameDir);
-		~EngineClass();
+		Engine(int argc, char* argv[], std::string_view gameDir);
+		~Engine();
 
 		void Run(Name starterLevel);
 	};
