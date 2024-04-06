09595efab211a7738303dbf10e3e8e1647095e64
LeoHellmig <220383@buas.nl>
2024-04-07 00:01:57 +0200

    Changed flycam system, added Input::GetDeltaMousePosition()

--- Source/Systems/UpdateFlyCamSystem.cpp
+++ Source/Systems/UpdateFlyCamSystem.cpp
@@ -28,9 +28,9 @@ void CE::UpdateFlyCamSystem::Update(World& world, float dt)
 
 	Input& input = Input::Get();
 
-	movementInput[Axis::Forward] = input.GetKeyboardAxis(Input::KeyboardKey::W, Input::KeyboardKey::S) + -input.GetGamepadAxis(0, Input::GamepadAxis::StickLeftY);
-	movementInput[Axis::Up] = input.GetKeyboardAxis(Input::KeyboardKey::E, Input::KeyboardKey::Q);;
-	movementInput[Axis::Right] =  input.GetKeyboardAxis(Input::KeyboardKey::D, Input::KeyboardKey::A) + input.GetGamepadAxis(0, Input::GamepadAxis::StickLeftX);
+	movementInput[Axis::Forward] = input.GetKeyboardAxis(Input::KeyboardKey::W, Input::KeyboardKey::S) * input.IsKeyboardKeyHeld(Input::KeyboardKey::LeftAlt) + -input.GetGamepadAxis(0, Input::GamepadAxis::StickLeftY);
+	movementInput[Axis::Up] = input.GetKeyboardAxis(Input::KeyboardKey::E, Input::KeyboardKey::Q) * input.IsKeyboardKeyHeld(Input::KeyboardKey::LeftAlt);
+	movementInput[Axis::Right] =  input.GetKeyboardAxis(Input::KeyboardKey::D, Input::KeyboardKey::A) * input.IsKeyboardKeyHeld(Input::KeyboardKey::LeftAlt) + input.GetGamepadAxis(0, Input::GamepadAxis::StickLeftX);
 
 	const glm::vec3 timeScaledMovementInput = movementInput * dt;
 
@@ -40,10 +40,12 @@ void CE::UpdateFlyCamSystem::Update(World& world, float dt)
 		Axis::Up
 	};
 
+	const glm::vec2 mouseInput = input.GetDeltaMousePosition() / world.GetViewport().GetViewportSize() * 100.0f * input.IsMouseButtonHeld(Input::MouseButton::Right, true);
+
 	const glm::vec2 rotationInput
 	{
-		input.GetKeyboardAxis(Input::KeyboardKey::ArrowDown, Input::KeyboardKey::ArrowUp) + input.GetGamepadAxis(0, Input::GamepadAxis::StickRightY),
-		input.GetKeyboardAxis(Input::KeyboardKey::ArrowRight, Input::KeyboardKey::ArrowLeft) + input.GetGamepadAxis(0, Input::GamepadAxis::StickRightX),
+		mouseInput.y + input.GetKeyboardAxis(Input::KeyboardKey::ArrowDown, Input::KeyboardKey::ArrowUp) + input.GetGamepadAxis(0, Input::GamepadAxis::StickRightY),
+		mouseInput.x + input.GetKeyboardAxis(Input::KeyboardKey::ArrowRight, Input::KeyboardKey::ArrowLeft) + input.GetGamepadAxis(0, Input::GamepadAxis::StickRightX)
 	};
 
 	const glm::vec2 timeScaledRotationInput = rotationInput * dt;
