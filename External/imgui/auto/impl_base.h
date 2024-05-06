815253c854aaf1b519cdb60e6c6876ad658d53b2
GuusKemperman <guus.kemperman@gmail.com>
2024-05-06 10:38:15 +0200

    Renamed vector to array

--- External/imgui/auto/impl_base.h
+++ External/imgui/auto/impl_base.h
@@ -152,7 +152,7 @@ IMGUI_AUTO_DEFINE_INLINE(template<typename ...Args>, const	std::tuple<Args...>, 
 #pragma region CONTAINERS
 #ifdef _VECTOR_
 IMGUI_AUTO_DEFINE_BEGIN(template<typename T>, std::vector<T>)
-	if(ImGui::detail::AutoContainerValues< std::vector<T> >("Vector " + name, var))
+	if(ImGui::detail::AutoContainerValues< std::vector<T> >("Array " + name, var))
 	{
 		ImGui::PushID(name.c_str());	ImGui::Indent();
 		ImGui::detail::AutoContainerPushBackButton(var); if (!var.empty()) ImGui::SameLine();
@@ -160,8 +160,8 @@ IMGUI_AUTO_DEFINE_BEGIN(template<typename T>, std::vector<T>)
 		ImGui::PopID();					ImGui::Unindent();
 	}
 IMGUI_AUTO_DEFINE_END
-IMGUI_AUTO_DEFINE_BEGIN(template<>, std::vector<bool>)
-	if(ImGui::detail::AutoContainerTreeNode< std::vector<bool> >("Vector " + name, var))
+IMGUI_AUTO_DEFINE_BEGIN(template<>, std::vector<bool>
+	if(ImGui::detail::AutoContainerTreeNode< std::vector<bool> >("Array " + name, var))
 	{
 		ImGui::Indent();
 		for (size_t i = 0; i < var.size(); ++i)
