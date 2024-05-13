f7b8660dd11e927d82069565e53ec3e3053f0057
Amália <220939@buas.nl>
2024-05-13 15:15:55 +0200

    Weapons working
    Not all variables used yet

--- Include/Systems/AbilitySystem.h
+++ Include/Systems/AbilitySystem.h
@@ -3,14 +3,19 @@
 
 namespace CE
 {
+	class AbilitiesOnCharacterComponent;
 	class CharacterComponent;
 	struct AbilityInstance;
+	struct WeaponInstance;
 
 	class AbilitySystem final :
 		public System
 	{
 	public:
 		void Update(World& world, float dt) override;
+
+		void IterateAbilitiesVector(AbilitiesOnCharacterComponent& abilities, CharacterComponent& characterData, entt::entity entity, World& world, float dt);
+		void IterateWeaponsVector(AbilitiesOnCharacterComponent& abilities, CharacterComponent& characterData, entt::entity entity, World& world, float dt);
 
 		// Checks for cooldowns/charges/requirements to see if an ability can be activated
 		static bool CanAbilityBeActivated(const CharacterComponent& characterData, const AbilityInstance& ability);
@@ -19,6 +24,13 @@ namespace CE
 		// calls the On Ability Activate Event and resets cooldowns and charges.
 		// Returns whether the ability was activated.
 		static bool ActivateAbility(World& world, entt::entity castBy, CharacterComponent& characterData, AbilityInstance& ability);
+
+		// Checks the same things CanAbilityBeActivated() plus extra weapon specific things.
+		static bool CanWeaponBeActivated(const CharacterComponent& characterData, const WeaponInstance& weapon);
+
+		//  
+		// Returns whether the ability was activated.
+		static bool ActivateWeapon(World& world, entt::entity castBy, CharacterComponent& characterData, WeaponInstance& weapon);
 
 	private:
 		friend ReflectAccess;
