82c5866fffcde2978123e54281e413fb1c94f66f
GuusKemperman <guus.kemperman@gmail.com>
2024-05-15 16:33:57 +0200

    Added gridspawner component

---
+++ Source/Components/GridSpawnerComponent.cpp
@@ -0,0 +1,152 @@
+#include "Precomp.h"
+#include "Components/GridSpawnerComponent.h"
+
+#include "Components/TransformComponent.h"
+#include "World/Registry.h"
+#include "World/World.h"
+#include "Assets/Prefabs/Prefab.h"
+#include "Utilities/Random.h"
+#include "Utilities/Reflect/ReflectComponentType.h"
+#include "Meta/ReflectedTypes/STD/ReflectVector.h"
+
+void CE::GridSpawner::OnConstruct(World&, entt::entity owner)
+{
+	mOwner = owner;
+}
+
+void CE::GridSpawner::SpawnGrid()
+{
+	if (mTiles.empty())
+	{
+		return;
+	}
+
+	World* const world = World::TryGetWorldAtTopOfStack();
+
+	if (world == nullptr)
+	{
+		return;
+	}
+
+	Registry& reg = world->GetRegistry();
+
+	TransformComponent* transform = reg.TryGet<TransformComponent>(mOwner);
+
+	if (transform == nullptr)
+	{
+		return;
+	}
+
+	for (const TransformComponent& child : transform->GetChildren())
+	{
+		reg.Destroy(child.GetOwner(), true);
+	}
+
+	float total{};
+
+	struct SpawnChance
+	{
+		AssetHandle<Prefab> mTile{};
+		float mChance{};
+	};
+	std::vector<SpawnChance> chances{};
+
+	for (uint32 i = 0; i < mTiles.size(); i++)
+	{
+		chances.emplace_back(SpawnChance{ mTiles[i], i < mSpawnChances.size() ? mSpawnChances[i] : 0.0f });
+	}
+
+	for (const SpawnChance spawnChance : chances)
+	{
+		total += spawnChance.mChance;
+	}
+
+	std::sort(chances.begin(), chances.end(),
+		[](const SpawnChance& Left, const SpawnChance& Right)
+		{
+			return Left.mChance < Right.mChance;
+		});
+
+	// Normalize and cumulate
+	float cumulative{};
+	for (SpawnChance& spawnChance : chances)
+	{
+		spawnChance.mChance /= total;
+
+		spawnChance.mChance += cumulative;
+		cumulative = spawnChance.mChance;
+	}
+	
+	for (uint32 x = 0; x < mWidth; x++)
+	{
+		for (uint32 y = 0; y < mHeight; y++)
+		{
+			glm::vec2 position =
+			{
+				static_cast<float>(x) * mSpacing.x,
+				static_cast<float>(y) * mSpacing.y
+			};
+
+			if (mIsCentered)
+			{
+				position.x -= static_cast<float>(mWidth - 1) * mSpacing.x * .5f;
+				position.y -= static_cast<float>(mHeight - 1) * mSpacing.y * .5f;
+			}
+
+			const double RandomNum = Random::Range(0.0f, 1.0f);
+
+			int32 BestIndex = 0;
+
+			for (int32 i = 1; i < chances.size(); i++)
+			{
+				const float Value = chances[i].mChance > RandomNum ? chances[i].mChance : INFINITY;
+				const float BestValue = chances[BestIndex].mChance > RandomNum ? chances[BestIndex].mChance : INFINITY;
+
+				if (Value < BestValue)
+				{
+					BestIndex = i;
+				}
+			}
+
+			AssetHandle<Prefab> tile = chances[BestIndex].mTile;
+
+			if (tile == nullptr)
+			{
+				continue;
+			}
+
+			const entt::entity entity = reg.CreateFromPrefab(*tile);
+
+			TransformComponent* child = reg.TryGet<TransformComponent>(entity);
+
+			if (child == nullptr)
+			{
+				continue;
+			}
+
+			child->SetParent(transform);
+			child->SetLocalPosition(position);
+		}
+	}
+}
+
+CE::MetaType CE::GridSpawner::Reflect()
+{
+	auto type = MetaType{ MetaType::T<GridSpawner>{}, "GridSpawner" };
+	MetaProps& props = type.GetProperties();
+	props.Add(Props::sIsScriptableTag);
+
+	type.AddField(&GridSpawner::mSpacing, "mSpacing").GetProperties().Add(Props::sIsScriptableTag);
+	type.AddField(&GridSpawner::mHeight, "mHeight").GetProperties().Add(Props::sIsScriptableTag);
+	type.AddField(&GridSpawner::mWidth, "mWidth").GetProperties().Add(Props::sIsScriptableTag);
+	type.AddField(&GridSpawner::mTiles, "mTiles").GetProperties().Add(Props::sIsScriptableTag);
+	type.AddField(&GridSpawner::mSpawnChances, "mChances").GetProperties().Add(Props::sIsScriptableTag);
+	type.AddField(&GridSpawner::mIsCentered, "mIsCentered").GetProperties().Add(Props::sIsScriptableTag);
+
+	type.AddFunc(&GridSpawner::SpawnGrid, "Spawn Grid").GetProperties().Add(Props::sCallFromEditorTag);
+
+	BindEvent(type, sConstructEvent, &GridSpawner::OnConstruct);
+
+	ReflectComponentType<GridSpawner>(type);
+	return type;
+}
