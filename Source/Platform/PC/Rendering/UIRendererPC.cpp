0d2d3aa0ee4910d4479d323284da51b040b55f4f
Karolina Motužytė <karolina.motuzyte02@gmail.com>
2024-05-27 10:19:08 +0200

    Fixed issues with transparent ui

--- Source/Platform/PC/Rendering/UIRendererPC.cpp
+++ Source/Platform/PC/Rendering/UIRendererPC.cpp
@@ -25,17 +25,18 @@ CE::UIRenderer::UIRenderer()
     CD3DX12_DEPTH_STENCIL_DESC depth = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
     depth.DepthFunc = D3D12_COMPARISON_FUNC_ALWAYS;
 
-    auto blendDesc = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+    CD3DX12_BLEND_DESC blendDesc = {};
+    blendDesc.AlphaToCoverageEnable = FALSE;
+    blendDesc.IndependentBlendEnable = FALSE;
     blendDesc.RenderTarget[0].BlendEnable = TRUE;
-    blendDesc.RenderTarget[0].LogicOpEnable = FALSE;
     blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
     blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
     blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
     blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
-    blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+    blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ONE;
     blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
-    blendDesc.RenderTarget[0].LogicOp = D3D12_LOGIC_OP_NOOP;
     blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+    blendDesc.RenderTarget[0].LogicOpEnable = FALSE;
 
     mPipeline = DXPipelineBuilder()
     .AddInput("POSITION", DXGI_FORMAT_R32G32B32_FLOAT, 0)
