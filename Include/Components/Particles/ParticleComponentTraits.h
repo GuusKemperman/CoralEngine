fd226d2c71bc3a6bd677236ccf0773578aed4696
GuusKemperman <guus.kemperman@gmail.com>
2024-05-29 16:34:48 +0200

    Particles are now simulated at 60 fps

--- Include/Components/Particles/ParticleComponentTraits.h
+++ Include/Components/Particles/ParticleComponentTraits.h
@@ -4,6 +4,6 @@ namespace CE
 {
 	namespace Particles
 	{
-		static inline constexpr std::optional<float> sParticleFixedTimeStep = std::nullopt;
+		static inline constexpr std::optional<float> sParticleFixedTimeStep = 1 / 60.0f;
 	}
 }
