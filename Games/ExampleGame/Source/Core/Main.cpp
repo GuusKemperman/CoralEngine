6a186762b555e14f58a5ea5c15657117dede1894
GuusKemperman <guus.kemperman@gmail.com>
2024-06-05 21:29:51 +0200

    Reverted some undesired changes

--- Games/ExampleGame/Source/Core/Main.cpp
+++ Games/ExampleGame/Source/Core/Main.cpp
@@ -9,30 +9,7 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc >= 2
-		&& strcmp(argv[1], "run_tests") == 0)
-	{
-		try
-		{
-			CE::Engine engine{ argc, argv, GAME_DIR };
-			engine.Run("L_MainMenu");
-		}
-		catch (const std::exception& e)
-		{
-			std::cout << e.what() << std::endl;
-			return 1;
-		}
-		catch (...)
-		{
-			std::cout << "Unknown exception" << std::endl;
-			return 1;
-		}
-	}
-	else
-	{
-		CE::Engine engine{ argc, argv, GAME_DIR };
-		engine.Run("L_MainMenu");
-	}
-
+	CE::Engine engine{ argc, argv, GAME_DIR };
+	engine.Run("L_MainMenu");
 	return 0;
 }
