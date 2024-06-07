795758581b9581ca231839e29ea395f1ff6901be
GuusKemperman <guus.kemperman@gmail.com>
2024-06-07 19:36:15 +0200

    Optimized swarming

--- Include/Components/Pathfinding/SwarmingTargetComponent.h
+++ Include/Components/Pathfinding/SwarmingTargetComponent.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Meta/MetaReflect.h"
+#include "Utilities/ASync.h"
 #include "Utilities/Geometry2d.h"
 
 namespace CE
@@ -10,22 +11,22 @@ namespace CE
 		float mDesiredRadius = 150.0f;
 		int mNumberOfSmoothingSteps = 2;
 
-		// The space between cells,
-		// and the space used for
-		// local avoidance.
-		float mSpacing{};
-
-		glm::vec2 mCellsTopLeftWorldPosition{};
-		int mFlowFieldWidth{};
-
-		std::vector<glm::vec2> mFlowField{};
-
-		TransformedAABB GetCellBox(const int x, const int y) const
+		struct FlowField
 		{
-			const glm::vec2 cellMin = glm::vec2{ static_cast<float>(x), static_cast<float>(y) } * mSpacing + mCellsTopLeftWorldPosition - glm::vec2{ mSpacing * .5f };
-			const glm::vec2 cellMax = cellMin + glm::vec2{ mSpacing };
-			return { cellMin, cellMax };
-		}
+			TransformedAABB GetCellBox(const int x, const int y) const
+			{
+				const glm::vec2 cellMin = glm::vec2{ static_cast<float>(x), static_cast<float>(y) } *mSpacing + mCellsTopLeftWorldPosition - glm::vec2{ mSpacing * .5f };
+				const glm::vec2 cellMax = cellMin + glm::vec2{ mSpacing };
+				return { cellMin, cellMax };
+			}
+
+			glm::vec2 mCellsTopLeftWorldPosition{};
+			int mFlowFieldWidth{};
+			float mSpacing{};
+
+			std::vector<glm::vec2> mFlowField{};
+		};
+		FlowField mCurrent{};
 
 	private:
 		friend ReflectAccess;
