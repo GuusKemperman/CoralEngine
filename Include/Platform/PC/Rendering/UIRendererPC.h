a2a996dd2f0c6f9140716e267885aaeda03d4690
Karolina Motužytė <224672@buas.nl>
2024-06-18 15:24:10 +0200

    Text rendering pc (#730)
    * Midway font loading
    
    * Font loading done
    
    * Sending correct data to gpu
    
    * Text on screen
    
    * Color can be changed in editor
    
    * Text rendering
    
    * Fixed indices
    
    * Fixed spacing
    
    * Fixed font uploading
    
    * Sorting text
    
    * Text sorting
    
    * Removed unnecessary vectors from gpuWordPC.cpp
    
    * Removed unnecessary vectors
    
    * Text gamma correction
    
    * Requested changes
    
    ---------
    
    Co-authored-by: Marcin Zalewski <220549@buas.nl>

--- Include/Platform/PC/Rendering/UIRendererPC.h
+++ Include/Platform/PC/Rendering/UIRendererPC.h
@@ -34,7 +34,22 @@ namespace CE
             glm::mat4 mTransposed;
         };
 
+        struct DrawRequest
+        {
+            entt::entity mEntity{};
+            float mDepth{};
+            uint16 mQuadIndex;
+            bool mIsText;
+        };
+
+
+        void RenderUI(const World& world, const DrawRequest& drawRequest);
+        void RenderText(const World& world, const DrawRequest& drawRequest);
+        void ProcessDrawRequests(const World& world);
+
     private:
         ComPtr<ID3D12PipelineState> mPipeline;
+        ComPtr<ID3D12PipelineState> mTextPipeline;
+        std::vector<DrawRequest> mDrawRequests;
     };
 }
--- Source/Platform/PC/Rendering/TexturePC.cpp
+++ Source/Platform/PC/Rendering/TexturePC.cpp
@@ -46,12 +46,92 @@ CE::Texture::Texture(AssetLoadInfo& loadInfo) :
 {
 }
 
-CE::Texture::Texture(std::string_view name, uint32_t, uint32_t, const unsigned char*) :
+CE::Texture::Texture(std::string_view name, uint32_t width, uint32_t height, const unsigned char* pixels) :
 	Asset(name, MakeTypeId<Texture>()),
 	mImpl(new DXImpl()),
 	mLoadedPixels(std::make_shared<STBIPixels>())
 {
-	// TODO: Implement for text rendering
+	mWidth = width;
+	mHeight = height;
+	Device& engineDevice = Device::Get();
+	ID3D12Device5* device = reinterpret_cast<ID3D12Device5*>(engineDevice.GetDevice());
+	ID3D12GraphicsCommandList4* uploadCmdList = reinterpret_cast<ID3D12GraphicsCommandList4*>(engineDevice.GetUploadCommandList());
+
+	bool commandListReset = engineDevice.StartUploadCommands();
+
+	DXGI_FORMAT dxgiformat = (DXGI_FORMAT)DXGI_FORMAT_R8G8B8A8_UNORM;
+	CD3DX12_RESOURCE_DESC resourceDescription = {};
+	resourceDescription.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	resourceDescription.Alignment = 0;
+	resourceDescription.Width = width;
+	resourceDescription.Height = height;
+	resourceDescription.DepthOrArraySize = 1;
+	resourceDescription.MipLevels = 1;
+	resourceDescription.Format = dxgiformat;
+	resourceDescription.SampleDesc.Count = 1;
+	resourceDescription.SampleDesc.Quality = 0;
+	resourceDescription.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	resourceDescription.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+
+	CD3DX12_HEAP_PROPERTIES heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+	mImpl->mTextureBuffer = std::make_unique<DXResource>(device, heapProperties, resourceDescription, nullptr, "Texture Buffer Resource Heap");
+
+	UINT64 textureUploadBufferSize;
+	device->GetCopyableFootprints(&resourceDescription, 0, 1, 0, nullptr, nullptr, nullptr, &textureUploadBufferSize);
+
+	const int bitsPerPixel = [&resourceDescription]
+		{
+			switch (resourceDescription.Format)
+			{
+			case DXGI_FORMAT_R32G32B32A32_FLOAT:
+				return 128;
+			case DXGI_FORMAT_R16G16B16A16_FLOAT:
+			case DXGI_FORMAT_R16G16B16A16_UNORM:
+				return 64;
+			case DXGI_FORMAT_R8G8B8A8_UNORM:
+			case DXGI_FORMAT_B8G8R8A8_UNORM:
+			case DXGI_FORMAT_B8G8R8X8_UNORM:
+			case DXGI_FORMAT_R10G10B10_XR_BIAS_A2_UNORM:
+			case DXGI_FORMAT_R10G10B10A2_UNORM:
+			case DXGI_FORMAT_R32_FLOAT:
+				return 32;
+			case DXGI_FORMAT_B5G5R5A1_UNORM:
+			case DXGI_FORMAT_B5G6R5_UNORM:
+			case DXGI_FORMAT_R16_FLOAT:
+			case DXGI_FORMAT_R16_UNORM:
+				return 16;
+			case DXGI_FORMAT_R8_UNORM:
+			case DXGI_FORMAT_A8_UNORM:
+				return 8;
+			default:
+				return 0;
+			}
+	}();
+
+	int bytesPerRow = (width * bitsPerPixel) / 8; // number of bytes in each row of the image data
+
+	D3D12_SUBRESOURCE_DATA textureData = {};
+	textureData.pData = pixels; // pointer to our image data
+	textureData.RowPitch = bytesPerRow; // size of all our triangle vertex data
+	textureData.SlicePitch = bytesPerRow * height; // also the size of our triangle vertex data
+
+	mImpl->mTextureBuffer->CreateUploadBuffer(device, static_cast<int>(textureUploadBufferSize), 0);
+	mImpl->mTextureBuffer->Update(uploadCmdList, textureData, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, 0, 1);
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Format = resourceDescription.Format;
+	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	mImpl->mHeapSlot = engineDevice.GetDescriptorHeap(RESOURCE_HEAP)->AllocateResource(mImpl->mTextureBuffer.get(), &srvDesc);
+
+	mImpl->mMipmapCB = nullptr;
+
+	if(commandListReset)
+		engineDevice.SubmitUploadCommands();
+
+	mLoadedPixels.reset();
 }
 
 CE::Texture::Texture(Texture&& other) noexcept = default;
